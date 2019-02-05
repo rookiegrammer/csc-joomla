b7083b025ffd0a03416d45985852ab1c6b03ee82
Deo Fetalvero <dmfetalvero@up.edu.ph>
2019-02-06 07:24:28 +0800

    foundation.work

---
+++ html/com_content/article/default.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_content
+ *
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+JHtml::addIncludePath(JPATH_COMPONENT . '/helpers');
+
+// Create shortcuts to some parameters.
+
+// $params  = $this->item->params;
+// $images  = json_decode($this->item->images);
+// $urls    = json_decode($this->item->urls);
+// $canEdit = $params->get('access-edit');
+// $user    = JFactory::getUser();
+// $info    = $params->get('info_block_position', 0);
+
+$item = $this->item;
+$user = JFactory::getUser();
+
+$package = array('item'=>$item, 'user'=>$user);
+
+// $this->item->category_alias
+$html = JLayoutHelper::render('custom.'.$this->item->category_alias, $package);
+if (empty($html)) {
+	echo JLayoutHelper::render('custom.default', $package);
+} else {
+	echo $html;
+}
+
+?>
---
+++ html/layouts/custom/default.php
@@ -0,0 +1,2 @@
+Hello
+<?php var_dump($displayData["item"]) ?>
