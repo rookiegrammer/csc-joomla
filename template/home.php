0b1a9b6786cc405cf36e6a092f2bc34b2c0b0e87
Deo Fetalvero <dmfetalvero@up.edu.ph>
2019-02-09 15:24:49 +0800

    database query for article of category

--- template/home.php
+++ template/home.php
@@ -1,1 +1,21 @@
-<jdoc:include type="component" />
+<?php
+
+function get_content_from_category($category) {
+  $db = JFactory::getDbo();
+
+  $query = $db->getQuery(true);
+  $subQuery = $db->getQuery(true);
+
+  $subQuery ->select('id')
+          	->from($db->quoteName('#__categories'))
+          	->where($db->quoteName('alias') . ' = ' . $db->quote($category));
+
+  $query ->select('*')
+         ->from($db->quoteName('#__content'))
+         ->where($db->quoteName('catid') . ' IN (' . $subQuery . ')');
+  $db->setQuery($query);
+  return $db->loadObjectList();
+}
+
+  var_dump(get_content_from_category('blog'));
+?>
