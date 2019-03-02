5a7199e55ad0498b705d85127a0719473bbb200a
rookiegrammer <30749394+rookiegrammer@users.noreply.github.com>
2019-03-02 18:37:23 +0800

    push design

--- template/home.php
+++ template/home.php
@@ -1,6 +1,6 @@
 <?php
 
-function get_content_from_category($category) {
+function get_content_from_category($category, $limit = 0) {
   $db = JFactory::getDbo();
 
   $query = $db->getQuery(true);
@@ -12,10 +12,21 @@ function get_content_from_category($category) {
 
   $query ->select('*')
          ->from($db->quoteName('#__content'))
-         ->where($db->quoteName('catid') . ' IN (' . $subQuery . ')');
+         ->where($db->quoteName('catid') . ' IN (' . $subQuery . ')')
+         ->order($db->quoteName('id').' DESC');
+
+  if (!empty($limit)) {
+    $query ->setLimit($limit);
+  }
+  
   $db->setQuery($query);
   return $db->loadObjectList();
 }
-
   var_dump(get_content_from_category('blog'));
+  var_dump(get_content_from_category('event'));
+  var_dump(get_content_from_category('news'));
+  var_dump(get_content_from_category('page-quick'));
+  var_dump(get_content_from_category('page'));
+  var_dump(get_content_from_category('publication'));
+  var_dump(get_content_from_category('staff'));
 ?>
