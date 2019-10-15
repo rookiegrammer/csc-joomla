5cfa1e77c4ca7dc8e444fefd54b07a6143eb981d
TjDMC <tjustin.asdf@outlook.com>
2019-10-15 17:36:30 +0800

    featured publications ux update

--- template/home.php
+++ template/home.php
@@ -315,12 +315,14 @@ function get_recent_publications($limit = 0, $featured = false){
     </div>
   </div>
     <script>
+        var cancel = false;
         $.fn.attachDragger = function(){
             var attachment = false, lastPosition, position, difference;
             $(this).on("mousedown mouseup mousemove",function(e){
                 if( e.type == "mousedown" ) attachment = true, lastPosition = [e.clientX, e.clientY];
                 if( e.type == "mouseup" ) attachment = false;
                 if( e.type == "mousemove" && attachment == true ){
+                    cancel = true;
                     position = [e.clientX, e.clientY];
                     difference = [ (position[0]-lastPosition[0]), (position[1]-lastPosition[1]) ];
                     $(this).scrollLeft( $(this).scrollLeft() - difference[0] );
@@ -335,6 +337,12 @@ function get_recent_publications($limit = 0, $featured = false){
         $(document).ready(function(){
             $("#recentScroll").attachDragger();
             $("#featuredScroll").attachDragger();
+            $(".stretched-link").click(function(e){
+                cancel && e.preventDefault();
+            });
+            $(".stretched-link").on("mousedown",function(e){
+                cancel=false;
+            });
         });
     </script>
   
